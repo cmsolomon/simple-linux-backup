9443923180dd426af6739ff77c510d670b591c83
chris solomon <chris.solomon@eroad.com>
2023-02-13 13:56:19 +1300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # simple-linux-backup
 Simple script to create backups of your files in linux.
+
+## Why does this exist
+There are lots of backup products out there, so why did I write this script?
+I made this because I am too lazy to wade through the instructions on the more fully featured products out there.
+
+## Prerequisites 
+bash, rsync, tr, sort
+Should be preinstalled, or simply installable, for most distros.
+
+
+## What it does
+This backup script creates uncompressed copies of the files in the selected directory (recursively), into a timestamp named backup folder.
+The script manages rotation of an arbitrary number of backups, updating the oldest.
+If the backup is interrupted (unexpected reset, crash or whatever) it will resume the incomplete backup (rather than moving on to the next oldest).
+
+The backups are uncompressed copies, so there is no 'restore' option - if you want to get a file back that you've lost, just get it from the backup folder.
+
+If you are backing up to a NAS, it may be worth investigating adding the z flag to the rsync call.
+
+## Configuration
+### MAX_BACKUPS
+You can configure the number of backups you want to maintain. If you specify 4 and you run the backup once per week, you will have 1 month of backups (but it will require 4 times the size of one backup).
+
+### BACKUP_PREFIX
+You can specify where the backup folders should be stored. You can specify a different location to the media that's hosting the script.
+If the BACKUP_PREFIX doesn't contain $USER and more than one user runs the backup script (e.g. several people using the same removable drive) then backups will be lost because only MAX_BACKUPS will be maintained.
+If you do include $USER, then each user will have MAX_BACKUPS seperately.
+
+### FOLDER_TO_BACKUP
+You can specify the folder to backup. By default, the $USERs home directory.
+
+## How to use it
+### Quickstart
+Copy the backup.sh script to the partition you want to backup to.
+Run the script
+./backup.sh
+A backup of your whole home directory will be made on the drive you ran the script on.
+(Don't run it in a folder in your home director, or things could get interesting.)
+
---
+++ backup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -e # Exit on first error
+
+# Backup Manager
+# Will maintain the specified number of backups.
+# If there are too many backups, the oldest will be deleted.
+# If there are fewer backups than specified a new backup will be made (this takes longer as all data has to be archived).
+# If there are the specified number of backups, the oldest will be updated (incremental update only has to copy the changes, so is faster).
+# The backup in progress has a special "work in progress" name. If a backup is found with this name, that backup will be assumed to be
+# the oldest and will be resumed (unless there are too many)
+# The work in progress backup will be renamed to the backup prefix + the epoch timestamp in seconds.
+
+# NOTE: If the BACKUP_PREFIX doesn't contain the $USER, then data can be lost if multiple users use the backup manager
+
+# Configuration Section
+readonly MAX_BACKUPS=3
+readonly BACKUP_PREFIX="backup/"$USER"/"
+
+readonly FOLDER_TO_BACKUP=$HOME
+
+# Constants - change them and it's your responsibility to ensure nothing breaks :) 
+readonly BACKUP_IN_PROGRESS="$BACKUP_PREFIX""WIP"
+
+# *****************************************************************************
+
+echo Backup Manager running...
+
+# Check if backup WIP exists
+backupInProgress="false"
+if [ -d "$BACKUP_IN_PROGRESS" ]; then
+    backupInProgress="true"
+fi
+
+# Find backup folders
+declare -a bups=()
+for backup in "$BACKUP_PREFIX"*/; do
+    bups+=("$backup")
+done
+backups=($(echo ${bups[*]}| tr " " "\n" | sort -n))
+
+if [ "$backupInProgress" == "true" ]; then
+    # If there are too many backups, remove the WIP backup as that will be the oldest
+    if [ "${#backups[@]}" -gt "$MAX_BACKUPS" ]; then
+        printf "Removing previous WIP backup, as there are more backups (%s) than defined number (%s)...\n\r" ${#backups[@]} $MAX_BACKUPS 
+        rm -rf "$BACKUP_IN_PROGRESS"
+        # The wip backup dir is always last
+        unset 'backups[-1]'
+        backupInProgress="false"
+    else
+        echo Resuming previously interrupted backup...
+    fi
+fi
+
+# If number of backup folders is greater than number of backups to maintain, remove oldest until within limits
+while [ "${#backups[@]}" -gt "$MAX_BACKUPS" ]; do
+    printf "Removing old backup %s as there are more backups (%s) than the defined number (%s)...\n\r" ${backups[0]} ${#backups[@]} $MAX_BACKUPS
+    rm -rf "${backup[0]}"
+    unset 'backups[0]'
+done
+
+# Now we have to get the WIP directory ready - if there is a backup in progress we can skip this part
+if [ "$backupInProgress" == "false" ]; then
+    # If we have the maximum number of backups allowable, rename the oldest to WIP
+    if [ "${#backups[@]}" -eq "$MAX_BACKUPS" ]; then
+        echo Renaming ${backups[0]} to $BACKUP_IN_PROGRESS...
+        mv ${backups[0]} "$BACKUP_IN_PROGRESS"
+    else
+        # Else create the new backup folder
+        printf "Defined number of backups (%s) not reached (only have %s backups), creating new backup...\n\r" $MAX_BACKUPS ${#backups[@]}
+        mkdir -p "$BACKUP_IN_PROGRESS"
+    fi
+fi
+
+# Now finally run the backup into the WIP directory
+echo Running backup...
+
+rsync -hua --no-inc-recursive --delete --force --info=progress2 $FOLDER_TO_BACKUP/ $BACKUP_IN_PROGRESS
+# Flags:
+# h - human readable sizes (KB, MG, GB), rather than everything in bytes.
+# u - update
+# a - Archive, implying:
+#                      recusive (r),
+#                      copy simlinks (l),
+#                      preserve permissions (p),
+#                      preserve modification times (t),
+#                      preserve group (g),
+#                      preserve owner (o),
+#                      preserve special device files and special files (D)
+# --no-inc-recursive - disable incremental recursion (calculate whole changeset before starting, not one dir at a time) - improves progress percentage accuracy. 
+# --delete - delete files that are missing from the destination on update
+# --force  - delete non-empty directories, if they are missing from destination on update
+# --info=progress2 - display the new overall progress indicator
+
+# Finally rename the WIP folder to using the epoch timestamp
+readonly newBackupName="$BACKUP_PREFIX""$(date +%s)"
+echo Renaming "$BACKUP_IN_PROGRESS" to "$newBackupName"...
+mv "$BACKUP_IN_PROGRESS" "$newBackupName"
+
+echo Backup complete.
